a4c673db6fc5b0f427e48c91b210d767aa3ae8ad
PhamMinhHieu1801 <phamhieu08928@gmail.com>
2021-12-05 07:39:21 +0700

    add comments

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,7 +1,12 @@
 import { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
-import PostDetail from '../components/post/post-detail';
+import Header from '../components/post/header';
+import Image from '../components/post/image';
+import Body from '../components/post/body';
+import Footer from '../components/post/footer'
+// import Actions from '../components/post/actions.js';
+import Comments from '../components/post/comments';
 import { useRef } from 'react';
 
 export default function Post() {
@@ -13,7 +18,23 @@ export default function Post() {
     const commentInput = useRef(null);
     const handleFocus = () => commentInput.current.focus();
       
-    return (
-        <PostDetail post={post}/>
-    );
+        return (
+          <div className="rounded row border bg-white border-gray-primary mb-12">
+            <Header username={post?.author} avatarSrc={post?.author_avatar} date={post?.create_date} post={post}/>
+            <Image src={post?.image_url}/>
+            <Body postId={post?.postId} title={post?.title} content={post?.content} post={post}/>
+            <Footer votes={post?.vote_numbers} comments={post?.comment_numbers}/>
+            {/* <Actions
+              postId={post?.postId}
+              handleFocus={handleFocus}
+            /> */}
+      
+            <Comments
+              postId={post?.postId}
+              comments={post?.comments}
+              postTime={post?.create_date}
+              commentInput={commentInput}
+            />      
+          </div>
+        );
 }
