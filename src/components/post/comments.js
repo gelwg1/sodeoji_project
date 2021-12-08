ba7c69bac80ea8bbd6513ad527f719a1f6763be8
Nguyen Minh Hieu <vampirehutbin@gmail.com>
2021-12-08 09:14:31 +0700

    feature-comments

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -5,11 +5,11 @@ import CommentForm from './comment-form';
 import { getCommentsByPostId, snapshotToArray, deleteCommentApi, getCommentByCommentId } from '../../services/firebase';
 import FirebaseContext from '../../context/firebase';
 
-export default function Comments({ postId, user}) {
+export default function Comments({ postId, user }) {
   const [backendComments, setBackendComments] = useState([]);
   const [activeComment, setActiveComment] = useState(null);
   const { database, storage } = useContext(FirebaseContext);
- 
+
   // var rootComments = [];
   // var listComments = database.ref(`Posts/${postId}/comments`).once('value', (snapshot) => {
   //   if (snapshot.exists()){
@@ -17,31 +17,26 @@ export default function Comments({ postId, user}) {
   //      rootComments = snapshotToArray(snapshot);
   //   };
   // });
-  var rootComments = database.ref(`Posts/${postId}`).once('value', snapshot => { 
-      
-        if (snapshot.exists()){
-          console.log(snapshot.val().comments);
-        }
-    });
+  const [rootComments, setRootComments] = useState();
 
 
   const getReplies = commentId => {
     return backendComments
-    .filter(backendComments => backendComments.parentId === commentId)
-    .sort(
-      (a,b) => 
-        new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-    );
+      .filter(backendComments => backendComments.parentId === commentId)
+      .sort(
+        (a, b) =>
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+      );
   };
- 
+
 
   const addComment = (text, parentId) => {
     var userId = user?.user_id;
     var username = user?.username;
-    if(parentId===undefined){
-      parentId='';
+    if (parentId === undefined) {
+      parentId = '';
     }
-  
+
     var commentId = database
       .ref(`Posts/${postId}/comments`)
       .push({
@@ -53,14 +48,14 @@ export default function Comments({ postId, user}) {
         username: username,
         create_date: Date.now(),
         vote_numbers: 0
-        })
+      })
       .key;
-    var commentRef = `Posts/${postId}/comments/` + commentId; 
+    var commentRef = `Posts/${postId}/comments/` + commentId;
     database
       .ref(commentRef)
       .update({
         id: commentId
-    });
+      });
     var comment = database.ref(`Posts/${postId}/comments`).child(commentId).once('value', (snapshot) => {
       const commentValue = snapshot.val();
       setBackendComments([commentValue, backendComments]);
@@ -77,46 +72,45 @@ export default function Comments({ postId, user}) {
         );
         setBackendComments(updateBackendComments);
       })
-    }    
+    }
   };
-  
-  // console.log(getCommentsByPostId(postId));
-  // useEffect(() => {
-  //   const rootComments = getCommentsByPostId(postId);
-    
-  //   console.log("list comment ", listComments);
-  //   if (listComments.length>0){
-  //     console.log("list comment ", listComments);
-  //     for(var i=0; i < listComments.length; i++){
-  //       listComments[i].then((data) => {
-  //         setBackendComments(data);
-  //         console.log("list comment i", listComments[i], "data", data);
-  //       });
-  //     }
-  //   }     
-  // }, [rootComments]);
+
+  // console.log(rootComments);
+  useEffect(() => {
+    async function getComments() {
+      await database.ref(`Posts/${postId}/comments`).once('value', snapshot => {
+
+        if (snapshot.exists()) {
+          setRootComments(snapshotToArray(snapshot));
+          // console.log(rootComments);
+        }
+      });
+    }
+
+    getComments();
+  }, rootComments?.length);
 
 
   return (
-      <div className="comments p-4 pt-1 pb-4">
-        <h3 className="comments-title">Comments</h3>
-        <div className="comments-container">
-          {/* {rootComments?.map((rootComment) => (
-            <Comment 
-              key={rootComment?.id} 
-              comment={rootComment} 
-              replies={getReplies(rootComment.id)}
-              user={user} 
-              deleteComment={deleteComment}
-              activeComment={activeComment}
-              setActiveComment={setActiveComment}
-              addComment={addComment}
-            />
-          ))} */}
-        </div>
-        <div className="comment-form-title">Write Comment</div>
-        <CommentForm submitLabel="Write" handleSubmit={addComment} />
+    <div className="comments p-4 pt-1 pb-4">
+      <h3 className="comments-title">Comments</h3>
+      <div className="comments-container">
+        {rootComments ? (rootComments.map((rootComment) => (
+          <Comment
+            key={rootComment?.id}
+            comment={rootComment}
+            replies={getReplies(rootComment.id)}
+            user={user}
+            deleteComment={deleteComment}
+            activeComment={activeComment}
+            setActiveComment={setActiveComment}
+            addComment={addComment}
+          />
+        ))) : null}
       </div>
+      <div className="comment-form-title">Write Comment</div>
+      <CommentForm submitLabel="Write" handleSubmit={addComment} />
+    </div>
   );
 }
 
